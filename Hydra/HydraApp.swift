537779524137af37cd743c90d50d7fefdc258e92
entertrainment <douglas@entertrainment.co.uk>
2025-04-14 18:36:51 +0100

    Initial Commit

---
+++ Hydra/HydraApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydraApp.swift
+//  Hydra
+//
+//  Created by DH on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HydraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydraTests/HydraTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydraTests.swift
+//  HydraTests
+//
+//  Created by DH on 14/04/2025.
+//
+
+import Testing
+@testable import Hydra
+
+struct HydraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
